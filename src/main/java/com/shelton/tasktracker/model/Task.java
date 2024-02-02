0cbba3669e95ac6ba04aad0f54543f699d148164
Shelton Bilgates <sheltonbilgates@gmail.com>
2024-02-02 18:47:22 +0530

    updated properties for mongoDB

--- src/main/java/com/shelton/tasktracker/model/Task.java
+++ src/main/java/com/shelton/tasktracker/model/Task.java
@@ -5,12 +5,16 @@ package com.shelton.tasktracker.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(value = "task")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
 	@Id
 	private String id;
