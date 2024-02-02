cb62217e8255783fb14590442aed7b7be473f1a4
Shelton Bilgates <sheltonbilgates@gmail.com>
2024-02-02 19:29:15 +0530

    cors

--- src/main/java/com/shelton/tasktracker/controller/TaskController.java
+++ src/main/java/com/shelton/tasktracker/controller/TaskController.java
@@ -4,20 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.shelton.tasktracker.dto.TaskTO;
 import com.shelton.tasktracker.model.Task;
 import com.shelton.tasktracker.service.TaskService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/task")
 public class TaskController {
 	
@@ -48,5 +42,7 @@ public class TaskController {
 		return taskService.updateTask(task);
 		
 	}
+
+
 }
   
